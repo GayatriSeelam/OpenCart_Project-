7105e420903ea4d2e944ea7c9ac09b9afc2468ce
GayatriSeelam <gayatrireddyseelam@gmail.com>
2025-01-03 14:33:51 +0530

    second commit

---
+++ src/test/java/testCases/TC004_search.java
@@ -0,0 +1,15 @@
+package testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import pageObjects.HomePage;
+import pageObjects.LoginPage;
+import pageObjects.MyAccountPage;
+import testBase.BaseClass;
+
+public class TC004_search extends BaseClass{
+//	implement later
+	
+
+}
